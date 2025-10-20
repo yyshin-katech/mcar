7da4961dcaa8045f571153bc0fc9168ae55603f7
KATECH-ADS-ioniq-ev <katech-ads@katech.re.kr>
2025-10-20 16:52:29 +0900

    jh j

--- src/localization/gps_system_localizer/src/to_control_team_demo.py
+++ src/localization/gps_system_localizer/src/to_control_team_demo.py
@@ -388,62 +388,62 @@ class DistanceCalculator(object):
                 self.occupied_count = 0
 
             # 20251014
-            distances = np.zeros(len(mapx_set))
-            for i in range(1, len(mapx_set)):
-                dx = mapx_set[i] - mapx_set[i-1]
-                dy = mapy_set[i] - mapy_set[i-1]
-                distances[i] = distances[i-1] + np.sqrt(dx**2 + dy**2)
+            # distances = np.zeros(len(mapx_set))
+            # for i in range(1, len(mapx_set)):
+            #     dx = mapx_set[i] - mapx_set[i-1]
+            #     dy = mapy_set[i] - mapy_set[i-1]
+            #     distances[i] = distances[i-1] + np.sqrt(dx**2 + dy**2)
 
-            # 각 축에 대한 스플라인 생성
-            cs_x = CubicSpline(distances, mapx_set, bc_type='natural')
-            cs_y = CubicSpline(distances, mapy_set, bc_type='natural')
+            # # 각 축에 대한 스플라인 생성
+            # cs_x = CubicSpline(distances, mapx_set, bc_type='natural')
+            # cs_y = CubicSpline(distances, mapy_set, bc_type='natural')
 
-            # 현재 웨이포인트의 거리
-            current_closest_waypoint_in_MATLAB = min(current_closest_waypoint_in_MATLAB, len(mapx_set) - 1)
-            current_distance = distances[current_closest_waypoint_in_MATLAB]
-
-            # 경로의 접선 벡터 계산
-            dx_ds = cs_x.derivative()(current_distance)
-            dy_ds = cs_y.derivative()(current_distance)
-
-            # 경로의 yaw 계산
-            path_yaw = np.arctan2(dy_ds, dx_ds)
-
-            # Yaw 오차 계산 (각도 차이를 -π ~ π 범위로 정규화)
-            yaw_error = path_yaw - yaw
-            yaw_error = np.arctan2(np.sin(yaw_error), np.cos(yaw_error))  # -π ~ π 범위로
-            yaw_error_size = abs(yaw_error)
-
-            p.yaw_error_size = yaw_error_size
-
-            # cs = CubicSpline(mapx_set, mapy_set, bc_type='natural')
-            # cs_derivative = cs.derivative()
+            # # 현재 웨이포인트의 거리
             # current_closest_waypoint_in_MATLAB = min(current_closest_waypoint_in_MATLAB, len(mapx_set) - 1)
-            # path_yaw = cs_derivative(mapx_set[current_closest_waypoint_in_MATLAB])
-            # yaw_error_size = abs(path_yaw - yaw)
+            # current_distance = distances[current_closest_waypoint_in_MATLAB]
+
+            # # 경로의 접선 벡터 계산
+            # dx_ds = cs_x.derivative()(current_distance)
+            # dy_ds = cs_y.derivative()(current_distance)
+
+            # # 경로의 yaw 계산
+            # path_yaw = np.arctan2(dy_ds, dx_ds)
+
+            # # Yaw 오차 계산 (각도 차이를 -π ~ π 범위로 정규화)
+            # yaw_error = path_yaw - yaw
+            # yaw_error = np.arctan2(np.sin(yaw_error), np.cos(yaw_error))  # -π ~ π 범위로
+            # yaw_error_size = abs(yaw_error)
 
             # p.yaw_error_size = yaw_error_size
 
-            # # 현재 주행할 경로쪽으로 방향이 제대로 맞으면 오토모드 송출 아니면, 수동모드 송출 ##
+            # # cs = CubicSpline(mapx_set, mapy_set, bc_type='natural')
+            # # cs_derivative = cs.derivative()
+            # # current_closest_waypoint_in_MATLAB = min(current_closest_waypoint_in_MATLAB, len(mapx_set) - 1)
+            # # path_yaw = cs_derivative(mapx_set[current_closest_waypoint_in_MATLAB])
+            # # yaw_error_size = abs(path_yaw - yaw)
 
-            if yaw_error_size < ODD_YAW_ERR_THRESHOLD:
-                # rospy.loginfo("On ODD")
-                p.Wrong_Way_Warn = 0
-            elif yaw_error_size > np.deg2rad(135):  #반대방향
-                p.On_ODD = 1
-                p.Road_State = 2
-                p.Wrong_Way_Warn = 1
-                p.distance_out_of_ODD = 0
-            else:   # 단순 이탈
-                p.On_ODD = 1
-                p.Road_State = 2
-                p.Wrong_Way_Warn = 0
-                p.distance_out_of_ODD = 0
+            # # p.yaw_error_size = yaw_error_size
 
-            # if yaw_error_size >= ODD_YAW_ERR_THRESHOLD :
+            # # # 현재 주행할 경로쪽으로 방향이 제대로 맞으면 오토모드 송출 아니면, 수동모드 송출 ##
+
+            # if yaw_error_size < ODD_YAW_ERR_THRESHOLD:
+            #     # rospy.loginfo("On ODD")
+            #     p.Wrong_Way_Warn = 0
+            # elif yaw_error_size > np.deg2rad(135):  #반대방향
             #     p.On_ODD = 1
             #     p.Road_State = 2
+            #     p.Wrong_Way_Warn = 1
             #     p.distance_out_of_ODD = 0
+            # else:   # 단순 이탈
+            #     p.On_ODD = 1
+            #     p.Road_State = 2
+            #     p.Wrong_Way_Warn = 0
+            #     p.distance_out_of_ODD = 0
+
+            # # if yaw_error_size >= ODD_YAW_ERR_THRESHOLD :
+            # #     p.On_ODD = 1
+            # #     p.Road_State = 2
+            # #     p.distance_out_of_ODD = 0
                 
 
         p.lane_name = current_lane_name
@@ -455,7 +455,7 @@ class DistanceCalculator(object):
         p.lateral_offset = current_d
         
         if p.LINK_ID == 56:
-            if distance_to_lane_end < 60.0:
+            if p.distance_to_lane_end < 60.0:
                 p.host_east = 0
                 p.host_north = 0
                 p.GPS_Over = 1
