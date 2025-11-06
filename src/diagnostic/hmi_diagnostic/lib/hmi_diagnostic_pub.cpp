a2b858ece277b66e9242919f6cde67b9253e7966
Yuyeong Shin <yyshin@katech.re.kr>
2025-11-06 19:12:55 +0900

    hmi diag period 0.1s

--- src/diagnostic/hmi_diagnostic/lib/hmi_diagnostic_pub.cpp
+++ src/diagnostic/hmi_diagnostic/lib/hmi_diagnostic_pub.cpp
@@ -5,7 +5,7 @@ HMI_DIAGNOSTIC_PUB::HMI_DIAGNOSTIC_PUB()
     pub = nh.advertise<katech_diagnostic_msgs::hmi_diagnostic_msg>("/diagnostic/hmi", 1);
     // sub = nh.subscribe("/katri_spat", 5, &HMI_DIAGNOSTIC_PUB::hmi_callback, this);
 
-    timer_ = nh.createTimer(ros::Duration(1.0), &HMI_DIAGNOSTIC_PUB::timer_callback, this);
+    timer_ = nh.createTimer(ros::Duration(0.1), &HMI_DIAGNOSTIC_PUB::timer_callback, this);
 }
 
 HMI_DIAGNOSTIC_PUB::~HMI_DIAGNOSTIC_PUB()
