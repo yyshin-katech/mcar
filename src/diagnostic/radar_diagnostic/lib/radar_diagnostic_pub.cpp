b836cf03ad5202f0fcdba09b7ca65e13bc96d87e
Yuyeong Shin <yyshin@katech.re.kr>
2025-11-07 15:25:08 +0900

    radar diag period 0.1s

--- src/diagnostic/radar_diagnostic/lib/radar_diagnostic_pub.cpp
+++ src/diagnostic/radar_diagnostic/lib/radar_diagnostic_pub.cpp
@@ -5,7 +5,7 @@ RADAR_DIAGNOSTIC_PUB::RADAR_DIAGNOSTIC_PUB()
     pub = nh.advertise<katech_diagnostic_msgs::radar_diagnostic_msg>("/diagnostic/radar", 1);
     sub = nh.subscribe("/sensors/front_radar", 5, &RADAR_DIAGNOSTIC_PUB::radar_object_callback, this);
 
-    timer_ = nh.createTimer(ros::Duration(1.0), &RADAR_DIAGNOSTIC_PUB::timer_callback, this);
+    timer_ = nh.createTimer(ros::Duration(0.1), &RADAR_DIAGNOSTIC_PUB::timer_callback, this);
 
     radar_callback_cnt = 0;
     radar_callback_cnt_old = 0;
